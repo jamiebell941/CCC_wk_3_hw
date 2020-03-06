e1ab60077c60f7f122576e87f13e6d794bb5ea3f
jamiebell941 <jamiebell941@gmail.com>
2020-03-06 12:12:40 +0000

    Set up for MVP

---
+++ models/customer.rb
@@ -0,0 +1,13 @@
+class Customer
+
+attr_reader :id
+attr_accessor :name, :funds
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds']
+end
+
+
+end
---
+++ models/ticket.rb
@@ -0,0 +1,10 @@
+class Ticket
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @film_id = options['film_id'].to_i
+    @customer_id = options['customer_id'].to_i
+  end
+
+
+end
